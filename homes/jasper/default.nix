53758a12124b2d780ff0885591ad143e70957add
Jappie3 <jasper22034@gmail.com>
2023-08-28 21:28:40 +0200

    feat: add git config

--- homes/jasper/default.nix
+++ homes/jasper/default.nix
@@ -16,9 +16,32 @@
     programs = {
       # let Home Manager manage itself when in standalone mode
       home-manager.enable = true;
+      
       direnv = {
         enable = true;
         nix-direnv.enable = true;
+      };
+
+      git = {
+        enable = true;
+        userName = "Jappie3";
+        userEmail = "jasper22034@gmail.com";
+	package = pkgs.gitFull;
+	extraConfig = {
+          init.defaultBranch = "master";
+	  url = {
+            "https://github.com/".insteadOf = "github:";
+	    "ssh://git@github.com/".pushInsteadOf = "github:";
+            "https://gitlab.com/".insteadOf = "gitlab:";
+            "ssh://git@gitlab.com/".pushInsteadOf = "gitlab:";
+            "https://aur.archlinux.org/".insteadOf = "aur:";
+            "ssh://aur@aur.archlinux.org/".pushInsteadOf = "aur:";
+            "https://git.sr.ht/".insteadOf = "srht:";
+            "ssh://git@git.sr.ht/".pushInsteadOf = "srht:";
+            "https://codeberg.org/".insteadOf = "codeberg:";
+            "ssh://git@codeberg.org/".pushInsteadOf = "codeberg:";
+	  };
+	};
       };
     };
 
