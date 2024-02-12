6564bebb34514c9f6b2c84c85d62b571cee9ceab
Jappie3 <jasper22034@gmail.com>
2024-02-12 20:23:59 +0100

    feat(secure_boot): remove options from module

--- modules/secure_boot.nix
+++ modules/secure_boot.nix
@@ -4,29 +4,26 @@
   config,
   inputs,
   ...
-}:
-with lib; {
+}: {
+  # implement Secure Boot using Lanzaboote
   imports = [inputs.lanzaboote.nixosModules.lanzaboote];
-  options.secure_boot.enable = mkEnableOption "Enable Secure Boot using Lanzaboote";
-  config = mkIf config.secure_boot.enable {
-    environment.systemPackages = [pkgs.sbctl];
-    boot = {
-      # see https://github.com/NixOS/rfcs/blob/master/rfcs/0125-bootspec.md
-      bootspec.enable = true;
+  environment.systemPackages = [pkgs.sbctl];
+  boot = {
+    # see https://github.com/NixOS/rfcs/blob/master/rfcs/0125-bootspec.md
+    bootspec.enable = true;
 
-      # https://github.com/nix-community/lanzaboote
-      lanzaboote = {
-        enable = true;
-        # sudo sbctl create-keys -> creates keys in /etc/secureboot
-        pkiBundle = "/etc/secureboot";
-      };
+    # https://github.com/nix-community/lanzaboote
+    lanzaboote = {
+      enable = true;
+      # sudo sbctl create-keys -> creates keys in /etc/secureboot
+      pkiBundle = "/etc/secureboot";
+    };
 
-      loader = {
-        # systemd-boot UEFI
-        efi.canTouchEfiVariables = true;
-        # lanzaboote replaces the systemd-boot module
-        systemd-boot.enable = lib.mkForce false;
-      };
+    loader = {
+      # systemd-boot UEFI
+      efi.canTouchEfiVariables = true;
+      # lanzaboote replaces the systemd-boot module
+      systemd-boot.enable = lib.mkForce false;
     };
   };
 }
