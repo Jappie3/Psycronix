d60109f5e40a3f8d9e5e46fce3867944bdf17e58
Jappie3 <jasper22034@gmail.com>
2024-04-18 01:46:11 +0200

    refactor(hm): move systemd user services to services.nix

--- homes/jasper/default.nix
+++ homes/jasper/default.nix
@@ -52,6 +52,7 @@ in let
 in {
   imports = [
     ./xdg.nix
+    ./services.nix
     ./graphical
     ./programs
     self.homeManagerModules.theme_config
@@ -166,39 +167,6 @@ in {
         fi
       '')
     ];
-  };
-
-  systemd.user = {
-    startServices = true;
-    services.load-theme = {
-      Install.WantedBy = ["default.target"];
-      Service = {
-        Restart = "always";
-        ExecStart = pkgs.writeShellScript "load-theme.sh" ''
-          set -e
-          THEME_FILE="/tmp/theme"
-          # only switch themes if the loc file exists
-          if [[ -e "${config.age.secrets.loc.path}" ]]; then
-            # check when sun rises & sets based on the current location
-            case "$(${pkgs.sunwait}/bin/sunwait poll $(<"${config.age.secrets.loc.path}"))" in
-              "NIGHT")
-                # load dark
-                "$(${pkgs.ripgrep}/bin/rg ExecStart /run/current-system/etc/systemd/system/home-manager-jasper.service | ${pkgs.coreutils}/bin/cut -d ' ' -f 2)/specialisation/dark/activate"
-                ${pkgs.coreutils}/bin/echo "dark" > "$THEME_FILE"
-              ;;
-              "DAY")
-                # load light
-                "$(${pkgs.ripgrep}/bin/rg ExecStart /run/current-system/etc/systemd/system/home-manager-jasper.service | ${pkgs.coreutils}/bin/cut -d ' ' -f 2)/specialisation/light/activate"
-                ${pkgs.coreutils}/bin/echo "light" > "$THEME_FILE"
-              ;;
-              *)
-                exit 1
-              ;;
-            esac
-          fi
-        '';
-      };
-    };
   };
 
   manual = {
