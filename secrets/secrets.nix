2b047d933bb1561723461fe3061d1ee88d7ac49e
Jappie3 <jasper22034@gmail.com>
2024-05-11 13:02:33 +0200

    chore(secrets): cleanup

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,17 +2,10 @@ let
   jasper = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA1mAN5Db7eZ0iuBGGxdPqQCR2l6jDZBjgX4ZVOcip27";
 
   Kainas = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIACMzy3iXxoPqMidD8ntdW1JQwjBXMQd+Fr0VvC5ftda";
-  systems = [Kainas];
+  Torii = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFlv9jSMCR6uILr0hQmud/Ki3qDoyZwuBJS7mn0vN+oU";
+  systems = [Kainas Torii];
 in {
-  # create new secret:
-  #agenix -e some-secret.age
-
-  # add secret to a NixOS module config:
-  #age.secrets.some-secret.file = ../secrets/some-necret.age;
-
-  # reference the mount path of the secret somewhere:
-  #config.age.secrets.some-secret.path
-
-  # define who can decrypt it:
-  #"some-secret.age".publicKeys = [jasper];
+  "jasper.age".publicKeys = [jasper systems];
+  "torii_ap.age".publicKeys = [jasper Torii];
+  "loc.age".publicKeys = [jasper];
 }
